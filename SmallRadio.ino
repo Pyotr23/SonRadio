2f8a052232734f155ab768359b575a06881d45c0
Pyotr <2chilavert3@mail.ru>
2019-09-06 00:05:12 +0300

    Не получается задать частоту и услышать станцию.

--- SmallRadio.ino
+++ SmallRadio.ino
@@ -2,7 +2,8 @@
 #include <Wire.h>
 #include <HardwareSerial.h>
 
-const uint8_t radioAddress = 0x10;
+const uint8_t ar1010Address = 0x10;
+
 
 void setup(){
     Wire.begin();
@@ -14,16 +15,42 @@ bool simpleFlag = false;
 void loop(){
     if (!simpleFlag){
         Serial.println("Go");
-        Wire.beginTransmission(radioAddress);         
-        Wire.write(0x1B);                  
-        Wire.endTransmission();
-
-        Wire.requestFrom(radioAddress, 2);
-        while (Wire.available() > 0){
-            Serial.print(Wire.read(), BIN);
-            Serial.println(Wire.read(), BIN);
-        }     
+        Serial.println(IsAR1010());
+        // uint16_t silverRain = (1 << 9) + 100110111;
+        writeBitToRegister(ar1010Address, 2, 0, 9);
+        writeToRegister(ar1010Address, 0x02, 100110111);
+        writeBitToRegister(ar1010Address, 2, 1, 9);
         Serial.println("End");      
         simpleFlag = true;
     }
+}
+
+bool IsAR1010(){
+    writeToRegister(ar1010Address, 0x1C);
+    Wire.requestFrom(ar1010Address, 2);
+    byte highByte, leastByte;
+    while(Wire.available()){
+        highByte = Wire.read();        
+        leastByte = Wire.read();        
+    }
+    uint16_t response = (highByte << 8) + leastByte;    
+    return response == 0x1010;
+}
+
+void writeToRegister(uint8_t address, int registerAddress){
+    Wire.beginTransmission(address);
+    Wire.write(registerAddress);
+    Wire.endTransmission();
+}
+
+void writeToRegister(uint8_t address, int registerAddress, uint16_t registerData){
+    Wire.beginTransmission(address);
+    Wire.write(registerAddress);
+    Wire.write(registerData);
+    Wire.endTransmission();
+}
+
+void writeBitToRegister(uint8_t address, int registerAddress, bool bitValue, byte shiftBits){
+    uint16_t loadData = bitValue << shiftBits;
+    writeToRegister(address, registerAddress, loadData);
 }
